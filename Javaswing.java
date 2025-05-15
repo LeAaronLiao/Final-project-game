4feab46429fc095a9112274fba06535bc4b82fa5
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-05-15 04:22:28 +0000

    maybe working gui

--- Javaswing.java
+++ Javaswing.java
@@ -1,32 +1,28 @@
-import javax.swing.*;        
+import javax.swing.*;
 
 public class Javaswing {
     /**
-     * Create the GUI and show it.  For thread safety,
+     * Create the GUI and show it. For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
     private static void createAndShowGUI() {
-        //Create and set up the window.
+        // Create and set up the window.
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
+        // Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello, World!");
         frame.getContentPane().add(label);
 
-        //Display the window.
-        frame.pack();
+        // Set the window size and display it.
+        frame.setSize(400, 300);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 }
