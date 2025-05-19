0bd5b8a40bc29d9c5c5e0ab164d3a91fb44353d3
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-05-19 23:24:08 +0000

    made some inheiting changes

--- Player.java
+++ Player.java
@@ -6,13 +6,13 @@ public class Player extends Entity {
     private int Energy;
     private RectanglePanel rect;
 
-    public Player(String name, int Health, int Level, int PositionX, int PositionY, int attackPower, int defensePower, String item, String[] inventory, int inventorySize, int Energy) {
+    public Player(String name, int Health, int Level, int PositionX, int PositionY, int attackPower, int defensePower, int someInt, String item, ArrayList<Item> inventory) {
         super(name, Health,Level, PositionX, PositionY);
         this.attackPower = attackPower;
         this.defensePower = defensePower;
         this.item = item;
         this.Energy = Energy;
-        this.rect = new RectanglePanel(x, y, 50, 50);
+        this.rect = new RectanglePanel(getX(), getY(), 50, 50);
     }
     public String getName() {
         return name;
@@ -37,20 +37,6 @@ public class Player extends Entity {
     }
     public void setHealth(int playerHealth) {
         this.health = playerHealth;
-    }
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
-    }  
-    public void moveX(int x) {
-        this.x += x;
-        this.rect.setX(this.x);
-    }
-    public void moveY(int y) {
-        this.y += y;
-        this.rect.setY(this.y);
     }
     public RectanglePanel getRect() {
         return this.rect;
